f2400742ea46254be7fa1ecd51e8a2ac5602eb78
“chandrasree” <chandrasree097@gmail.com>
2024-04-30 02:34:28 +0600

    banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Hero from "../component/Hero";
 
 const Home = () => {
   const navigateTo = useNavigate();
@@ -23,6 +24,10 @@ const Home = () => {
       });
   }, []);
   return (
+    <>
+    <div>
+    <Hero></Hero>
+    </div>
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-4">Countries</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -43,6 +48,7 @@ const Home = () => {
         ))}
       </div>
     </div>
+    </>
   );
 };
 
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className="carousel w-full">
+  <div id="slide1" className="carousel-item relative w-full" style={{ height: "600px" }}>
+    <img src="https://img.freepik.com/free-photo/aerial-view-city-by-water_23-2150893117.jpg?t=st=1714421565~exp=1714425165~hmac=c6aa0fe4819e10b788755b984c16dda2ac5b3008b7aebfa6cb0d37cc730b48b7&w=740" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://img.freepik.com/free-photo/parliament-budapest_1084-57.jpg?t=st=1714422204~exp=1714425804~hmac=7a29c119cbcaf9b473417808e829b19f87ba8209e7e0bfabd1e39643997b2685&w=996" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://img.freepik.com/free-photo/prague-castle-from-vltava-side_1398-2758.jpg?t=st=1714422442~exp=1714426042~hmac=6a0001fb514e4458f5917cdd16b599bdbaa14e5dd6feadb0aed929995afae57e&w=1060" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://img.freepik.com/free-photo/view-prague-by-night_268835-1129.jpg?t=st=1714422527~exp=1714426127~hmac=5bc124e414884bbf130acce3ecb8dfe9ea62f8f3cf6da7f1bfa90bfc8636f2a1&w=996" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+    </div>
   )
 }
 
